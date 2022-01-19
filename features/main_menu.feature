d3a7fc452ddf533458472c2eddc8a793e524b68c
tramalho <thiagofacul@gmail.com>
2022-01-19 20:00:02 -0300

    22. Create first Cucumber scenario

---
+++ features/main_menu.feature
@@ -0,0 +1,5 @@
+Feature: As a user I want convert units
+  Scenario: Tap Left Side Menu
+    When I tap on menu icon, I should see left side menu
+    Given I land on menu screen
+    Then I should see left side menu
---
+++ features/step_definitios/main_manu.rb
@@ -0,0 +1,11 @@
+When(/^I tap on menu icon, I should see left side menu$/) do
+  puts("step 01")
+end
+
+Given(/^I land on menu screen$/) do
+  puts("step 02")
+end
+
+Then(/^I should see left side menu$/) do
+  puts("step 03")
+end
